eff8eb695ab76f6a52302e28cc005d7171ce6238
veer <58711645+Singhzing-kid1@users.noreply.github.com>
2022-06-27 17:42:55 -0600

    reseting to do a 2.5D so i can have elevation.

--- isorpggame/Assets/Tilemaps/Tilesets/Test.tsx
+++
@@ -1,7 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.5" name="Test" tilewidth="32" tileheight="32" tilecount="1" columns="0">
- <grid orientation="orthogonal" width="1" height="1"/>
- <tile id="1">
-  <image width="32" height="32" source="Images/testingFullBlock.png"/>
- </tile>
-</tileset>
